fea7451db1ea292b6782c7ea5648e94458c8ab95
Hui <jeffrey.x.hui@gmail.com>
2020-04-10 09:06:46 -0700

    comp tests passing

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -1,5 +1,6 @@
 # The following list comprehension exercises will make use of the 
 # defined Human class. 
+from math import sqrt
 class Human:
     def __init__(self, name, age):
         self.name = name
@@ -7,6 +8,11 @@ class Human:
 
     def __repr__(self):
         return f"<Human: {self.name}, {self.age}>"
+
+    def modifyHuman(self):
+        self.name = self.name.upper()
+        self.age += 5
+        return self
 
 humans = [
     Human("Alice", 29),
@@ -24,48 +30,50 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
+a = [getattr(human, "name") for human in humans if getattr(human, "name")[0] == 'D']
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = []
+b = [getattr(human, "name") for human in humans if getattr(human, "name")[-1] == 'e']
 print(b)
-
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [getattr(human, "name") for human in humans if ord(getattr(human, "name")[0]) >= ord('C') and ord(getattr(human, "name")[0]) <= ord('G')]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = []
+d = [getattr(human, "age") + 10 for human in humans]
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = []
+e = [getattr(human, "name") + '-' + str(getattr(human, "age")) for human in humans]
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [(getattr(human, "name"), getattr(human, "age")) for human in humans if getattr(human, "age") >= 27 and getattr(human, "age") <= 32]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
+
+#I need to return a modified human object in each iteration
+#Without mutating t he original
 print("All names uppercase:")
-g = []
+g = [Human(human.name.upper(), human.age + 5) for human in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = []
+h = [math.sqrt(getattr(human, "age")) for human in humans]
 print(h)
--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,12 +4,18 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
-    # TODO
+    def drive(self):
+        return "vroooom"
 
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__(2)
 
+    def drive(self):
+        return "BRAAAP!!"
 # Subclass Motorcycle from GroundVehicle.
 #
 # Make it so when you instantiate a Motorcycle, it automatically sets the number
